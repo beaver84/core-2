184fd61125667ea4ebc4c65f7677efe112c8b350
bae kyungtae <b841010@naver.com>
2023-11-12 00:02:58 +0900

    Initial commit

---
+++ src/test/java/com/example/core2/order/OrderServiceTest.java
@@ -0,0 +1,21 @@
+package com.example.core2.order;
+
+import com.example.core2.member.Grade;
+import com.example.core2.member.Member;
+import com.example.core2.member.MemberService;
+import com.example.core2.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+    @Test
+    void createOrder() {
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
