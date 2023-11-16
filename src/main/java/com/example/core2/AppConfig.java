ee58113ed6564985429c8e7a2b9b7e7ef4836f0f
bae kyungtae <b841010@naver.com>
2023-11-16 23:07:34 +0900

    4장 스프링 컨테이너와 스프링 빈 업데이트

--- src/main/java/com/example/core2/AppConfig.java
+++ src/main/java/com/example/core2/AppConfig.java
@@ -1,7 +1,6 @@
 package com.example.core2;
 
 import com.example.core2.discount.DiscountPolicy;
-import com.example.core2.discount.FixDiscountPolicy;
 import com.example.core2.discount.RateDiscountPolicy;
 import com.example.core2.member.MemberRepository;
 import com.example.core2.member.MemberService;
@@ -9,21 +8,27 @@ import com.example.core2.member.MemberServiceImpl;
 import com.example.core2.member.MemoryMemberRepository;
 import com.example.core2.order.OrderService;
 import com.example.core2.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy() {
-//        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
 }
